aca3bba7c09388698fd1eb81d3c8ff483ad8dc89
Bradford Folkens <bfolkens@gmail.com>
2012-04-04 09:49:44 -0500

    print_buttons option was removed in pull request #3, now restored

--- app/models/spree/print_invoice_configuration.rb
+++ app/models/spree/print_invoice_configuration.rb
@@ -1,5 +1,6 @@
 module Spree
   class PrintInvoiceConfiguration < Preferences::Configuration
+    preference :print_buttons, :string, :default => 'invoice'
     preference :print_invoice_logo_path, :string, :default => Spree::Config[:admin_interface_logo]
   end
 end
